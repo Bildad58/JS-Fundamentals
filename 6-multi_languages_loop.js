2972f59181f2e6fb00f26b556c592b24b589ac9d
bildad58 <bildadawasioya@gmail.com>
2025-05-24 07:19:15 +0300

    loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const c = "C is fun";
+const py ="Python is cool";
+const js = "JavaScript is amazing";
+
+const sentence= [c, py, js];
+
+for (let i = 0; i < 3; i++) {
+  console.log(sentence[i]);
+}
