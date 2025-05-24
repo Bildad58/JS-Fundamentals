99fbd09c760c16d3a7505a7ff7bd3494caecf3c7
bildad58 <bildadawasioya@gmail.com>
2025-05-24 06:52:52 +0300

    arg

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,4 +1,10 @@
-if (arg[0] === undefined) {
+const py = "Python is cool";
+const c ="C is fun";
+const h = "Html is good"
+
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
     console.log("No argument");
 }else {
     console.log(args[0]);
